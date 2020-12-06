03a591e807836c4e47e07a3589a6fa18e1286ef5
Vincent <jamesvincentsiauw@gmail.com>
2020-12-06 20:24:15 +0700

    cors

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from controller import process_data
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict_price():
